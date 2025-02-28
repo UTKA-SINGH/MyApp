28cedb9105d995cf56dff431f618d19698839adc
UTKA-SINGH <singh.utkarsh4007@gmail.com>
2025-02-28 19:53:29 +0530

    Changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import { Header } from '../Components/Header';
 import { Footer } from '../Components/Footer';
 import '../style/main.css';
-import Image1 from "../images/Untitled design.png";
+import Image1 from "../images/WhatsApp Image 2025-02-28 at 7.09.57 PM.jpeg";
 export function Home () {
     return (
         <div className="main-wrapper">
@@ -10,7 +10,7 @@ export function Home () {
             <div >
                 <h1>Hello!
                 <br></br>This is Utkarsh Singh,
-                <br></br>I am an Aspiring<strong> Software Developer.</strong>  
+                <br></br>I am a Aspiring<strong> Software Developer.</strong>  
                 </h1>
                 <img className = "Home-Image" alt="Utkarsh's_Photo" src = {Image1}/>
             </div>
